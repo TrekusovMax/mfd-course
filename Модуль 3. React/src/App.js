837bde8d28c657e5560034e2da1d25fca6601c73
Maxim <maxtrekusovsite@gmail.com>
2023-03-08 17:11:45 +0300

    task 2 done

--- Модуль 3. React/src/App.js
+++ Модуль 3. React/src/App.js
@@ -1,9 +1,17 @@
 import './App.css'
 
+import { useLocalStorage } from './useLocalStorage'
+
 function App() {
+  const [token, { setItem, removeItem }] = useLocalStorage('token')
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <div>Твой токен: {token}</div>
+        <button onClick={() => setItem('token1')}>Задать токен</button>
+        <button onClick={() => removeItem()}>Удалить токен</button>
+      </header>
     </div>
   )
 }
