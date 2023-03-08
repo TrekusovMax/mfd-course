e0a49fbd1fa07f9f9f27d74fd73727d9b26ebefb
Maxim <maxtrekusovsite@gmail.com>
2023-03-08 23:54:02 +0300

    additional task 2 done

--- Модуль 3. React/src/App.js
+++ Модуль 3. React/src/App.js
@@ -1,9 +1,14 @@
 import './App.css'
+import { useToggle } from './useToggle'
 
 function App() {
+  const [value, toggle] = useToggle(['blue', 'orange', 'cyan', 'teal'])
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h3> {value.toString()}</h3>
+        <button onClick={() => toggle()}>Toggle</button>
+      </header>
     </div>
   )
 }
