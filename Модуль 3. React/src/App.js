18aa712e6c1292d812b55ad1648b3d68d267cbe7
Maxim <maxtrekusovsite@gmail.com>
2023-03-08 22:14:41 +0300

    additional task 1 done

--- Модуль 3. React/src/App.js
+++ Модуль 3. React/src/App.js
@@ -1,9 +1,28 @@
 import './App.css'
+import { useWindowScroll } from './useWindowScroll'
 
 function App() {
+  //стиль для окна браузера, чтобы включить полосы прокрутки
+  const style = {
+    overflow: 'scroll',
+    height: '150vh',
+    width: '150vw',
+  }
+  const { scroll, scrollTo } = useWindowScroll()
+
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header" style={style}>
+        <div>
+          Scroll position x: {scroll.x}, y: {scroll.y}
+        </div>
+        <button
+          style={{ marginTop: '2rem', padding: '10px 15px' }}
+          onClick={() => scrollTo({ y: 0 })}
+        >
+          Scroll to top
+        </button>
+      </header>
     </div>
   )
 }
