b8a2cedd7d54a97580a5619e56054a309ba82a3c
Maxim <maxtrekusovsite@gmail.com>
2023-03-12 23:42:43 +0300

    added useFetch into hooks

--- Модуль 3. React/src/App.js
+++ Модуль 3. React/src/App.js
@@ -1,34 +1,14 @@
 import './App.css'
-import { useFetch } from './useFetch'
+import ReactTasks from './react-tasks/react-task'
 
 function App() {
-  const URL = 'https://jsonplaceholder.typicode.com/posts'
-
-  const { data, isLoading, error, refetch } = useFetch(URL)
-
   return (
     <div className="App">
       <header className="App-header">
-        <button
-          onClick={() =>
-            refetch({
-              params: {
-                _limit: 3,
-              },
-            })
-          }
-        >
-          Refetch
-        </button>
-        {isLoading && 'Загрузка...'}
-        {error && 'Произошла ошибка'}
-        {data &&
-          !isLoading &&
-          data.map((item) => <p key={item.id}>{item.title}</p>)}
+        <ReactTasks />
       </header>
     </div>
   )
 }
 
 export default App
-
