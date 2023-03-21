a001759f90e8661b30d292866c7d9d5166d4a4ed
Maxim <maxtrekusovsite@gmail.com>
2023-03-21 23:46:09 +0300

    added detail heroes page

--- Модуль 3. React/src/components/Routing.jsx
+++ Модуль 3. React/src/components/Routing.jsx
@@ -3,11 +3,15 @@ import HomePage from './pages/homePage'
 import Heroes from './pages/heroes'
 import Locations from './pages/locations'
 import Episodes from './pages/episodes'
+import HeroesDetail from './pages/heroesDetail'
 const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/heroes/:id?" element={<Heroes />} />
+      <Route path="/heroes">
+        <Route index element={<Heroes />} />
+        <Route path=":id" element={<HeroesDetail />} />
+      </Route>
       <Route path="/location" element={<Locations />} />
       <Route path="/episode" element={<Episodes />} />
       <Route path="*" element={<Navigate to="/" />} />
---
+++ Модуль 3. React/src/components/pages/heroesDetail.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import data from '../data/characters.json'
+const HeroesDetail = () => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+  const hero = data.filter((d) => d.id === Number(id))[0]
+
+  return (
+    <div className="grid mt-3 place-items-center grid-rows-3 grid-flow-col gap-3">
+      <div className="row-span-2 place-self-end">
+        <img
+          className=" mb-3 h-auto max-w-xl rounded-lg shadow-xl dark:shadow-gray-800"
+          src={`${hero.image}`}
+          alt={`${hero.name}`}
+        />
+      </div>
+      <div className=" col-span-3  ">
+        <button
+          onClick={() => navigate(-1)}
+          type="button"
+          className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+        >
+          Назад
+        </button>
+      </div>
+      <div className="col-span-1 place-self-center font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+        {`${hero.name}`}
+      </div>
+      <div className="row-span-1 col-span-1 ">
+        <div className=" max-w-md space-y-1 text-left  md:text-3xl lg:text-4xl">
+          <p>
+            <span className="font-bold">Status:</span> {`${hero.status}`}
+          </p>
+          <p>
+            <span className="font-bold">Species:</span> {`${hero.species}`}
+          </p>
+          {hero.type && (
+            <p>
+              <span className="font-bold">Type: </span>
+              {hero.type}
+            </p>
+          )}
+          <p>
+            <span className="font-bold">Gender: </span>
+            {`${hero.gender}`}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroesDetail
