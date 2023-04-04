5455395b1b0dda7bc7de4f3f05a20a24211cdd7e
Trekusov Max <maxtrekusovsite@gmail.com>
2023-04-04 17:19:47 +0300

    add error boundary

--- Модуль 3. React/src/react-tasks/react-router-task.jsx
+++ Модуль 3. React/src/react-tasks/react-router-task.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import NavBar from '../components/NavBar'
 import Routing from '../components/Routing'
 import { AuthProvider } from '../context/AuthProvider'
+import ErrorBoundary from './../components/ErrorBoundary'
 
 export default function ReactRouterTask() {
   return (
     <div className="container  mx-auto">
       <AuthProvider>
         <NavBar />
-        <Routing />
+        <ErrorBoundary>
+          <Routing />
+        </ErrorBoundary>
       </AuthProvider>
     </div>
   )
