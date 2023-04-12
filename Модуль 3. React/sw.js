d3809306ff4d287b5ba5de65bd193c92b96e2131
Trekusov Max <maxtrekusovsite@gmail.com>
2023-04-12 12:01:48 +0300

    added firebase cli

--- Модуль 3. React/sw.js
+++ Модуль 3. React/sw.js
@@ -1,15 +1,53 @@
 const staticCacheName = 'static-site'
-const assets = ['/']
+const dynamicCacheName = 'dinamic-site'
+const assets = [
+  '/src/pages/homePage.jsx',
+  '/src/App.css',
+  '/src/App.jsx',
+  '/src/components/NavBar.jsx',
+  '/assets/icons/icon-144x144.png',
+]
 
-self.addEventListener('install', (event) => {
-  console.log('Service Worker has been installed')
-  caches.open(staticCacheName).then((cache) => {
-    cache.addAll()
-  })
+self.addEventListener('install', async (event) => {
+  const cache = await caches.open(staticCacheName)
+  await cache.addAll(assets)
 })
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker has been activated')
+
+self.addEventListener('activate', async (event) => {
+  const cachesKeysArr = await caches.keys()
+  await Promise.all(
+    cachesKeysArr
+      .filter((key) => key !== staticCacheName && key !== dynamicCacheName)
+      .map(caches.delete(key)),
+  )
 })
+
 self.addEventListener('fetch', (event) => {
-  console.log(event.request.url)
+  event.respondWith(cacheFirst(event.request))
 })
+
+async function cacheFirst(request) {
+  const cached = await caches.match(request)
+  try {
+    return (
+      cached ??
+      (await fetch(request).then((response) => {
+        return networkFirst(request)
+      }))
+    )
+  } catch (e) {
+    return networkFirst(request)
+  }
+}
+
+async function networkFirst(request) {
+  const cache = await caches.open(dynamicCacheName)
+  try {
+    const response = await fetch(request)
+    await cache.put(request, response.clone())
+    return response
+  } catch (e) {
+    const cached = await cache.match(request)
+    return cached
+  }
+}
